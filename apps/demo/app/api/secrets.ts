a31b100c28dad80f4c000ff3549b4d092944bd75
vipinxsharma <vipinxsharma@gmail.com>
2026-03-31 19:01:01 +0530

    Refactor API keys to use environment variables
    Updated API keys to use environment variables for security.

--- apps/demo/app/api/secrets.ts
+++ apps/demo/app/api/secrets.ts
@@ -1,4 +1,4 @@
-export const API_KEY = "9495de9c-bb7c-11f0-a6ee-066a7fa2e369";
+export const API_KEY = process.env.LIVEAVATAR_API_KEY!;
 export const API_URL = "https://api.liveavatar.com";
 export const AVATAR_ID = "dd73ea75-1218-4ef3-92ce-606d5f7fbc0a";
 
@@ -13,5 +13,5 @@ export const CONTEXT_ID = "5b9dba8a-aa31-11f0-a6ee-066a7fa2e369";
 export const LANGUAGE = "en";
 
 // LITE MODE Customizations
-export const ELEVENLABS_API_KEY = "cdbf1cbb6c7d8e42ef5f658d190b8307a0976d0c77c59306eadbac4c4276be75";
-export const ANTHROPIC_API_KEY = "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";
+export const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY!;
+export const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY!;
